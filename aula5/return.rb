b6fa6d9e8fd0742f5aac47b3c0c269a8267ffc0f
Allan Souza <allanronaldo95@gmail.com>
2021-01-06 17:29:15 -0300

    Exemplo da função return dentro de um método

---
+++ aula5/return.rb
@@ -0,0 +1,14 @@
+# Função que compara dois números 
+def compare(a, b)
+    # Devolução de valores com return
+    return a > b
+end 
+
+a = 1
+b = 2
+
+# Chamada da função compare e atribuição 
+result = compare(a, b)
+
+# Exibição de resultados
+puts "O resultado da comparação é #{result}"
